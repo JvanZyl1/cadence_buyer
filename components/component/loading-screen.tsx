5f62d76812ebd538f2c386ffde92180e16a4a976
JvanZyl1 <J.R.vanZyl@student.tudelft.nl>
2024-07-25 11:35:27 +0530

    depbug

--- components/component/loading-screen.tsx
+++ components/component/loading-screen.tsx
@@ -17,6 +17,7 @@ To read more about using these font, please visit the Next.js documentation:
 - App Directory: https://nextjs.org/docs/app/building-your-application/optimizing/fonts
 - Pages Directory: https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
 **/
+import React from 'react';
 import Link from "next/link"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
